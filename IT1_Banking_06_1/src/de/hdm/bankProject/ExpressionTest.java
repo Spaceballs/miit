c4cabed09dd80107cda078fb3ff4a7846a1a11c8
Spaceballs <mail@sebastianfink.com>
2014-12-01 13:40:55 +0100

    update 5,6u7

---
+++ IT1_Banking_06_1/src/de/hdm/bankProject/ExpressionTest.java
@@ -0,0 +1,21 @@
+/**
+  *
+  * Beschreibung.
+  *
+  * @version 1.0 vom 13.11.2004
+  * @author CR
+  */
+
+public class ExpressionTest {
+
+  public static void main(final String[] args) {
+     int i;
+
+     i = 10;
+     System.out.println("Vor der Modifikation ist der Wert " + (i--%5>0));
+
+     i = 10;  // Achtung: der Wert von i muss wieder auf 10 gesetzt werden!
+     System.out.println("Nach der Umschreibung ist der Wert " + ((i-- % 5) > 0));
+  }
+}
+
